94be01df071bbbec3b9410b1bf474d250bc69c31
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-09-17 12:05:11 +0600

    Add New Project

---
+++ MyApp/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+export default function App() {
+
+  return <WelcomeScreen />;
+}
