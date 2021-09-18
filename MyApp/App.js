a3e4b64d8bfa10583319ad7dee918568d2a4a865
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-09-18 14:30:16 +0600

    Text Styles

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {
   View,
+  Text,
 } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+        }}
+      >I Love React Native</Text>
+    </View>
+  );
 }
---
+++ MyApp2/App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          color: "blue",
+          fontStyle: "italic",
+          // textTransform: "capitalize",
+          // textDecorationLine: "line-through",
+
+        }}
+      >I Love React Native</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
