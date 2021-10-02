2ef4bf7322b0b0669aef9b108853ef39580dcd2e
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-10-02 22:27:38 +0600

    Add Image

---
+++ PracticeApp/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+
+} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* <Text>Hello React Native!</Text> */}
+      <Image
+        //blurRadius={5}
+        //fadeDuration={1000}
+        style={styles.nature}
+        source={require("./assets/2.jpg")} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    //marginTop: 45,
+  },
+
+  nature: {
+    width: 250,
+    height: 350,
+  },
+});
