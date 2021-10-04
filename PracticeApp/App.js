031b1a58804559db78d0a4b904606f7c7c443361
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-10-04 22:33:35 +0600

    Update App.js
    Dimensions + scrollview + pressable + flex

--- PracticeApp/App.js
+++ PracticeApp/App.js
@@ -1,41 +1,331 @@
 import React from 'react';
 import {
   View,
-
+  Dimensions,
+  ScrollView,
+  Pressable,
 } from 'react-native';
 
 export default function App() {
 
+  const height = Dimensions.get('window').height / 5;
 
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-      }}
+
+    <ScrollView style={{
+      backgroundColor: "#fff",
+      flex: 1,
+      marginTop: 30,
+    }}
     >
 
-      <View
-        style={{
-          backgroundColor: "yellow",
-          flex: 1,
-        }}
-      />
+
+      {/* //first row start */}
 
       <View
         style={{
-          backgroundColor: "orange",
-          flex: 1,
+          flexDirection: "row",
         }}
-      />
+      >
+
+        <Pressable
+          onPress={() => alert("blue")}
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+
+        <Pressable
+          onPress={() => alert("tomato")}
+          style={{
+            backgroundColor: "tomato",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+      {/* //first row end */}
+
+      {/* //second row start */}
 
       <View
         style={{
-          backgroundColor: "tomato",
-          flex: 1,
+          flexDirection: "row",
         }}
-      />
+      >
 
-    </View>
+        <View
+          style={{
+            backgroundColor: "yellow",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+      {/* //second row end */}
+
+      {/* //third row start */}
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "tomato",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+      {/* //third row end */}
+
+      {/* //forth row start */}
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "tomato",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+      {/* //forth row end */}
+
+      {/* //fifth row start */}
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+      {/* //fifth row end */}
+
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "tomato",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "tomato",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "tomato",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+
+          }}
+        />
+
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            width: 200,
+            height: height,
+            margin: 5,
+          }}
+        />
+      </View>
+
+    </ScrollView>
   );
 }
+
