10f6bd7f37c9ba7081de52c8d0ebc76b1ff896a2
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-10-08 23:22:54 +0600

    Update App.js

--- PracticeApp/App.js
+++ PracticeApp/App.js
@@ -1,50 +1,33 @@
 import React from 'react';
 import {
   View,
-  Text,
 } from 'react-native';
+
 
 export default function App() {
 
   return (
-
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
       }}
     >
       <View
+
         style={{
-          height: 300,
-          width: 430,
-          backgroundColor: "green",
-          justifyContent: "center",
-          alignItems: "center",
+          backgroundColor: "orange",
+          width: 150,
+          height: 150,
+          //shadowColor: "#000",
+          //shadowOpacity: 1,
+          elevation: 50,
         }}
       >
-
-        <View
-          style={{
-            backgroundColor: "red",
-            width: 200,
-            height: 200,
-            borderRadius: 110,
-          }}
-        >
-
-        </View>
-
       </View>
-      <Text
-        style={{
-          fontSize: 30,
-        }}
-      >BANGLADESH</Text>
 
-    </View>
-
+    </View >
   );
 }
 
