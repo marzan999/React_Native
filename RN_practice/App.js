7d867ee54be14022c1071dce512d10ca88ec778b
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-10-29 22:23:22 +0600

    Adding a custom header

---
+++ RN_practice/App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header title="Hello React Native" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
