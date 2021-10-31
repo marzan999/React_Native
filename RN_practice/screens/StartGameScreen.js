237a81b64ab2b746a18a2e654b13300eb18dc564
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2021-10-31 20:59:42 +0600

    Cleaning User Input & Controlling the Soft Keyboard

--- RN_practice/screens/StartGameScreen.js
+++ RN_practice/screens/StartGameScreen.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button, } from 'react-native';
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
 import Colors from '../constants/Colors';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a new game</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a number</Text>
-                <Input style={styles.input}
-                    blurOnSubmit
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    keyboardType="numeric"
-                    //keyboardType="number-pad"
-                    //keyboardType="email-address"
-                    maxLength={2}
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a new game</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a number</Text>
+                    <Input style={styles.input}
+                        blurOnSubmit
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        //keyboardType="numeric"
+                        keyboardType="number-pad"
+                        //keyboardType="email-address"
+                        maxLength={2}
+                        onChangedText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                        <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
